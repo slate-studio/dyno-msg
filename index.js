caf93bda2e3519966752b3194570ba58ee9a3035
Alexander Kravets <alex@slatestudio.com>
2018-03-01 13:01:17 +0200

    Fix bundle issues

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 'use strict'
 
-module.exports = (config) => {
-  return msg(this.config)
-    .then(({ globals }) => {
-      global.Message  = globals.Message
-      global.Listener = globals.Listener
-    })
-}
+module.exports = require('./lib/msg')
